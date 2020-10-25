c293020f08f36421b339da6be6005d9ed96c3ffd
agnesferenczi <65777475+agnesferenczi@users.noreply.github.com>
2020-10-25 14:13:18 +0100

    működő carousel

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-//Buttons
-const previousBtn = document.querySelector('#previousBtn');
-const nextBtn = document.querySelector('#nextBtn');
-//Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-
-carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
-
-nextBtn.addEventListener('click',()=>{
-    if(counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = 'transform 0.2s ease-in-out';
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
-});
-
-previousBtn.addEventListener('click',()=>{
-    if(counter <= 0) return;
-    carouselSlide.style.transition = 'transform 0.2s ease-in-out';
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
-});
-
-
-
-console.log(carouselImages.length);
+$(document).ready(function(){
+    $('.carousel-slide').slick({});
+  });
